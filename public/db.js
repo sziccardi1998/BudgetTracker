15d4e1f45588278faa0104ea8d0ab745df3b437d
sziccardi1998 <sziccardifullstack@gmail.com>
2021-03-01 19:02:30 -0500

    Finish updates to db.js

--- public/db.js
+++ public/db.js
@@ -25,3 +25,33 @@ function saveRecord(record) {
 
   store.add(record);
 }
+
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+
+
+          const store = transaction.objectStore("pending");
+
+          store.clear();
+        });
+    }
+  };
+}
+
+window.addEventListener("online", checkDatabase); 
